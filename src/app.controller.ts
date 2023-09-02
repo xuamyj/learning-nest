1c01ea30b2fa475bc88664c7b0d5a68a81d24e9d
Amy Xu <xuamyj@gmail.com>
2023-09-02 03:34:43 -0400

    followed https://docs.nestjs.com/techniques/mvc

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Get, Controller, Render } from '@nestjs/common';
+// import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  // constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('index')
+  root() {
+    return { message: 'Does it reload by itself? (No)' };
+  }
+
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+}
